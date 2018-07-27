178d99d3569d226c98e5f8a11de5abab6b388146
root <root@localhost.localdomain>
2018-07-27 10:52:08 +0800

    day04

---
+++ day04/erp_parent/erp_web/src/main/java/cn/itcast/erp/action/OrderdetailAction.java
@@ -0,0 +1,19 @@
+package cn.itcast.erp.action;
+import cn.itcast.erp.biz.IOrderdetailBiz;
+import cn.itcast.erp.entity.Orderdetail;
+
+/**
+ * 订单明细Action 
+ * @author Administrator
+ *
+ */
+public class OrderdetailAction extends BaseAction<Orderdetail> {
+
+	private IOrderdetailBiz orderdetailBiz;
+
+	public void setOrderdetailBiz(IOrderdetailBiz orderdetailBiz) {
+		this.orderdetailBiz = orderdetailBiz;
+		super.setBaseBiz(this.orderdetailBiz);
+	}
+
+}
